194d423aa060a53754b2bc1519354b701d716783
Martin Fenner <martin.fenner@datacite.org>
2016-09-14 05:57:22 -0600

    don't put styles and locales under version control

--- vendor/docker/80_submodules.sh
+++ vendor/docker/80_submodules.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-rm -rf /home/app/webapp/public/styles
 git clone --depth=1 https://github.com/citation-style-language/styles.git public/styles
 rm -rf !$/.git
 
-rm -rf /home/app/webapp/public/locales
 git clone --depth=1 https://github.com/citation-style-language/locales.git public/locales
 rm -rf !$/.git
