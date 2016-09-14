97910c70b7419806636019693b6c8d984a88e4e6
Martin Fenner <martin.fenner@datacite.org>
2016-09-13 22:50:49 -0600

    don't use public folder for styles and locales

--- src/citeproc.js
+++ src/citeproc.js
@@ -6,11 +6,11 @@ var locales = {};
 
 function init() {
 	console.log("loading styles...");
-	styles = loadDir("./public/styles/", /(.*)\.csl$/);
+	styles = loadDir("./styles/", /(.*)\.csl$/);
 	console.log(Object.keys(styles).length + " styles loaded.");
 
 	console.log("loading locales...");
-	locales = loadDir("./public/locales/", /locales-(.*)\.xml$/);
+	locales = loadDir("./locales/", /locales-(.*)\.xml$/);
 	console.log(Object.keys(locales).length + " locales loaded.");
 }
 
